6e9090a6d284671910dc09920320acffcdea1122
Sam Karp <sfcypret@gmail.com>
2022-10-31 20:12:54 -0400

    create authentication functions

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,13 +2,16 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+// import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [login, { error }] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -17,6 +20,16 @@ const LoginForm = () => {
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+
+    try {
+      const { data } = await login ({
+        variables: { ...formState }
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    };
 
     // check if form has everything (as per react-bootstrap docs)
     const form = event.currentTarget;
@@ -85,6 +98,7 @@ const LoginForm = () => {
           Submit
         </Button>
       </Form>
+      {error && <div>Login failed</div>}
     </>
   );
 };
--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
+import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
@@ -26,9 +27,9 @@ const SignupForm = () => {
       const { data } = await addUser({
         variables: { ...formState }
       });
-      console.log(data);
+      Auth.login(data.addUser.token);
     } catch (e) {
-      console.log(e);
+      console.error(e);
     };
 
     // check if form has everything (as per react-bootstrap docs)
@@ -114,6 +115,7 @@ const SignupForm = () => {
           Submit
         </Button>
       </Form>
+      {error && <div> Sign up failed. </div>}
     </>
   );
 };
