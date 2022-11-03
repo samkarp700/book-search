81a94ba22d5a366c31528b23d00727e22cebb788
Sam Karp <sfcypret@gmail.com>
2022-11-03 19:01:39 -0400

    adjust connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/booksearch', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:127.0.0.1:27017/booksearch', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
