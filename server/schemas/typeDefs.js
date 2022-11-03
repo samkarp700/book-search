35f1556ce48f709f41f0923fc4c7bf45da3f8160
Sam Karp <sfcypret@gmail.com>
2022-11-03 09:51:25 -0400

    updating code - still trying to debug

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -19,7 +19,7 @@ const typeDefs = gql `
         link: String
 
     }
-    input bookData {
+    input BookInput {
         bookId: String!
         authors: String
         description: String
@@ -36,7 +36,7 @@ const typeDefs = gql `
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(input: bookData): User
+        saveBook(input: BookInput): User
         removeBook(bookId: String!): User
     }
 
