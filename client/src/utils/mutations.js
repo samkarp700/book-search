e52a82c80c14496071f8b2388e08da64c40178bf
Sam Karp <sfcypret@gmail.com>
2022-11-03 00:03:47 -0400

    debug attempts

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -26,7 +26,7 @@ export const ADD_USER = gql `
 `;
 
 export const SAVE_BOOK = gql `
-    mutation saveBook{$input: BookInput!} {
+    mutation saveBook{$input: bookData!} {
         saveBook(input: $input) {
             _id
             username
