52ba1040020597b26b2e84459cfd1896e790f21c
Sam Karp <sfcypret@gmail.com>
2022-11-02 22:47:55 -0400

    match typedefs to book data update typedefs for links

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -11,26 +11,33 @@ const typeDefs = gql `
         savedBooks: [Book]
     }
     type Book {
-        _id: ID
+        bookId: String!
         title: String
         authors: String
         description: String
-        image: ???
-        link: ???
+        image: String
+        link: String
+
+    }
+    input bookData {
+        bookId: String!
+        authors: String
+        description: String
+        title: String
+        image: String
+        Link: String
 
     }
     type Query {
         me: User
-        users: [User]
-        user(username: String!): User
-        books(username: String): [Book]
-        book(_id: ID!): Book
+        
     }
+
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(bookTitle: String!, bookId: ID!, image: ? link: ? ): User
-        removeBook(bookId: ID!): User
+        saveBook(input: bookData): User
+        removeBook(bookId: String!): User
     }
 
     type Auth {
